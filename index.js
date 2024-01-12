62088184fe47cb5da4e4b4d06a9325c89221d6cb
MarquesKev <kevinlucas2702@gmail.com>
2024-01-12 02:13:37 -0300

    correção de acentuação das palavras

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ if (level <= 1000){
     console.log("O herói de nome " + hero + " está no nível de Ferro.");
 
 } else if (level <= 2000){
-    console.log ("O herói de nome " + hero + " esta no nível de Bronze.")
+    console.log ("O herói de nome " + hero + " esta no nível de Bronze.");
 
 } else if (level <= 5000){
-    console.log ("O herói de nome " + hero + " esta no nível de Prata.")
+    console.log ("O herói de nome " + hero + " esta no nível de Prata.");
 
 } else if (level <= 7000){
-    console.log ("O herói de nome " + hero + " esta no nível de Ouro.")
+    console.log ("O herói de nome " + hero + " esta no nível de Ouro.");
 
 } else if (level <= 8000){
-    console.log ("O herói de nome " + hero + " esta no nível de Platina.")
+    console.log ("O herói de nome " + hero + " esta no nível de Platina.");
 
 } else if (level <= 9000){
-    console.log ("O herói de nome " + hero + " esta no nível de Ascendente.")
+    console.log ("O herói de nome " + hero + " esta no nível de Ascendente.");
 
 } else if (level <= 10000){
-    console.log ("O herói de nome " + hero + " esta no nível de Imortal.")
+    console.log ("O herói de nome " + hero + " esta no nível de Imortal.");
 
 } else {   
-    console.log ("O herói de nome " + hero + " esta no nível de Radiante.")
+    console.log ("O herói de nome " + hero + " esta no nível de Radiante.");
 
 }
