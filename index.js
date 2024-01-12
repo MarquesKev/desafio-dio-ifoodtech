40cfd0681852391d4ddf3075afa0e6635ea2cfa7
MarquesKev <kevinlucas2702@gmail.com>
2024-01-12 02:08:24 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var hero = "King Arthur"
+var level = "9400"
+
+if (level <= 1000){
+    console.log("O herói de nome " + hero + " está no nível de Ferro.");
+
+} else if (level <= 2000){
+    console.log ("O herói de nome " + hero + " esta no nível de Bronze.")
+
+} else if (level <= 5000){
+    console.log ("O herói de nome " + hero + " esta no nível de Prata.")
+
+} else if (level <= 7000){
+    console.log ("O herói de nome " + hero + " esta no nível de Ouro.")
+
+} else if (level <= 8000){
+    console.log ("O herói de nome " + hero + " esta no nível de Platina.")
+
+} else if (level <= 9000){
+    console.log ("O herói de nome " + hero + " esta no nível de Ascendente.")
+
+} else if (level <= 10000){
+    console.log ("O herói de nome " + hero + " esta no nível de Imortal.")
+
+} else {   
+    console.log ("O herói de nome " + hero + " esta no nível de Radiante.")
+
+}
